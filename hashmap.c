8b2a0cf9eb9b5fc5ec51ca540397b6e7987e4bfa
jan28f <163349875+jan28f@users.noreply.github.com>
2024-04-23 21:17:34 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -81,9 +81,9 @@ Pair * searchMap(HashMap * map,  char * key) {
 
 Pair *firstMap(HashMap *map) 
 {
-  if (map == NULL) return NULL;
+  if (map == NULL || map->current == -1) return NULL;
   
-  Pair *dato;
+  Pair *dato = NULL;
   for (long i = 0 ; i < map->capacity ; i++)
     if (map->buckets[i] != NULL)
     {
