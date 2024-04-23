a8044a6b408c00fc601f371f34b04a870b10c63e
jan28f <163349875+jan28f@users.noreply.github.com>
2024-04-23 22:00:03 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -82,7 +82,7 @@ Pair * searchMap(HashMap * map,  char * key) {
 
 Pair *firstMap(HashMap *map)
 {
-  if (map == NULL) return NULL;
+  if (map == NULL || map->size == 0) return NULL;
 
   for (long i = 0 ; i < map->capacity ; i++)
     if (map->buckets[i] != NULL)
@@ -96,7 +96,7 @@ Pair *firstMap(HashMap *map)
 
 Pair *nextMap(HashMap *map)
 {
-  if (map == NULL) return NULL;
+  if (map == NULL || map->size == 0) return NULL;
 
   for (long i = map->current + 1 ; i < map->capacity ; i++)
     if (map->buckets[i] != NULL)
