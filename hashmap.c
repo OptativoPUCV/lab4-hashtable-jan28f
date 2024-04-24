7a150823b1cd2054415ec91be6a288dbedd6d5ad
jan28f <163349875+jan28f@users.noreply.github.com>
2024-04-24 04:32:01 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -80,14 +80,16 @@ void enlarge(HashMap *map)
   map->capacity *= 2;
   map->buckets = (Pair **)malloc(sizeof(Pair *) * map->capacity);
   map->size = 0;
-
-  for (long i = 0 ; i < map->capacity ; i++)
-  {
-    if (antiguoBuckets[i] != NULL && antiguoBuckets[i]->key != NULL)
-      insertMap(map, antiguoBuckets[i]->key, antiguoBuckets[i]->value);
-    else
-      map->buckets[i] = NULL;
-  }
+  for (long i = 0; i < map->capacity / 2 ; i++)
+    {
+      if (antiguoBuckets[i] != NULL && antiguoBuckets[i]->key != NULL)
+      {
+        insertMap(map, antiguoBuckets[i]->key, antiguoBuckets[i]->value);
+        
+      }
+      
+    }
+  
 }
 
 HashMap * createMap(long capacity)
