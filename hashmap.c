4c5562b8eb14cb419b2d7824933200ef5fd94825
jan28f <163349875+jan28f@users.noreply.github.com>
2024-04-24 03:22:45 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -56,8 +56,11 @@ void insertMap(HashMap * map, char * key, void * value)
   }
 }
 
-void enlarge(HashMap * map) {
-    enlarge_called = 1; //no borrar (testing purposes)
+void enlarge(HashMap *map)
+{
+  enlarge_called = 1; //no borrar (testing purposes)
+
+  Pair **nuevoBuckets = map->buckets;
 }
 
 HashMap * createMap(long capacity)
