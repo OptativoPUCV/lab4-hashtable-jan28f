de3c4bc440ebe7a3b0e804fd22ed8060243e5b9e
jan28f <163349875+jan28f@users.noreply.github.com>
2024-04-24 03:05:40 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -134,7 +134,7 @@ Pair *nextMap(HashMap *map)
   if (map == NULL || map->size == 0) return NULL;
 
   for (long i = map->current + 1 ; i < map->capacity ; i++)
-    if (map->buckets[i] != NULL)
+    if (map->buckets[i] != NULL && map->buckets[i]->key != NULL)
     {
       map->current = i;
       return map->buckets[i];
